9ae768c96229cd7842fcd870f49cfd191a55c92c
Eden Denis <edendenis@gmail.com>
2025-08-15 09:54:18 -0300

    preparando o repo para o codex

--- docs/PREDICTIONS/MOLPAK/exchange.f90
+++
@@ -1,9 +0,0 @@
-      SUBROUTINE EXCHANGE (X,Y) 
-          
-      REAL :: X, Y, T
-      
-      T = X                                                              
-      X = Y                                                              
-      Y = T                                                              
-      RETURN                                                             
-      END SUBROUTINE EXCHANGE   
